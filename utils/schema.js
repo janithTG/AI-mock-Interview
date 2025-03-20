6681e4fdd5e7c547cb90d33a9046a2ab8377705f
janithTG <janith.thenura31@gmail.com>
2025-03-20 15:01:48 +0530

    sonner and schema added

--- utils/schema.js
+++ utils/schema.js
@@ -11,3 +11,15 @@ export const MockInterview = pgTable('mockInterview',{
     mockId:varchar('mockId').notNull()
 
 })
+
+export const UserAnswer = pgTable('UserAnswer',{
+    id:serial('id').primaryKey(),
+    mockIdRef:varchar('mockId').notNull(),
+    question:varchar('question').notNull(),
+    correctAns:varchar('correctAns'),
+    userAns:text('userAns'),
+    feedback:text('feedback'),
+    rating:varchar('rating'),
+    userEmail:varchar('userEmail'),
+    createdAt:varchar('createdAt')
+})
